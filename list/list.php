c36cb717da7f0342e30eaedb807e03885f348bea
allanppss <allanppss@gmail.com>
2016-09-26 17:27:03 -0300

    List PHP

---
+++ list/list.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+// Inclui o script de conexão.
+include  "../conexao.php";
+
+// Query responsável por listar registros da tabela.
+$query = mysql_query("SELECT * FROM tabela ORDER BY id DESC");
+
+while($n = mysql_fetch_array($query)){
+	echo $n['nome'] . '</br>';
+	echo $n['idade'] . '</br>';
+	echo $n['sexo'] . '</br>';
+	echo '-----------------';
+};
+
+
+// Fecha a conexão com o banco de dados
+mysql_close($conecta);
+
+?>
